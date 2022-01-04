f2d0d0fd8075a0f444583c82bd31f543204dcf31
Henrique Tavares de Lima <tavares@tavares.he-Latitude-3410>
2022-01-04 16:07:15 -0300

    estudo com o projeto mercadolivro

---
+++ src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
@@ -0,0 +1,37 @@
+package com.mercadolivro.controller
+
+import com.mercadolivro.controller.request.PostCustomerRequest
+import com.mercadolivro.model.CustomerModel
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("customer")
+class CustomerController {
+
+    val customers = mutableListOf<CustomerModel>()
+
+    @GetMapping
+    fun getCustomer(): List<CustomerModel> {
+        return customers
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    fun create(@RequestBody customer: PostCustomerRequest) {
+
+        val id = if (customers.isEmpty()) {
+            1
+        } else {
+            customers.last().id.toInt() + 1
+        }.toString()
+        customers.add(CustomerModel(id, customer.name, customer.email))
+    }
+
+
+}
